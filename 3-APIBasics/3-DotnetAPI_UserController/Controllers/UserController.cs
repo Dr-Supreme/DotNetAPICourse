c15b632cbb1ade00631058b680fd8a46a9a64579
IsaacTheScientist <61564345+Dr-Supreme@users.noreply.github.com>
2024-10-23 14:34:20 -0500

    Update UserController.cs

--- 3-APIBasics/3-DotnetAPI_UserController/Controllers/UserController.cs
+++ 3-APIBasics/3-DotnetAPI_UserController/Controllers/UserController.cs
@@ -7,14 +7,14 @@ namespace DotnetAPI.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    public UserController()
+    public UserController() // without the word public you wont be able to see the response because of an error
     {
 
     }
 
-    [HttpGet("GetUsers/{testValue}")]
-    // public IActionResult Test()
-    public string[] GetUsers(string testValue)
+    [HttpGet("GetUsers/{testValue}")] // this will show in our api with the {testValue}. so lets say testValue = iosaac the url will bet /test/iosaac
+    // public IActionResult Test() // api response
+    public string[] GetUsers(string testValue) //returning a sting array
     {
         string[] responseArray = new string[] {
             "test1",
